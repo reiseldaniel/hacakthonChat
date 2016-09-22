22529d7916a47b6f2ad6fba42161d5aad4963b6c
harel <harel@wisdo.com>
2016-09-22 17:33:18 +0300

    gg

--- src/screens/index.js
+++ src/screens/index.js
@@ -11,6 +11,13 @@ import TopTabsScreen from './TopTabsScreen';
 
 // register all screens of the app (including internal ones)
 export function registerScreens(store, Provider) {
+  Navigation.registerComponent('Main', () => LoginScreen, store, Provider);
+  Navigation.registerComponent('Campaign', () => LoginScreen, store, Provider);
+  Navigation.registerComponent('Proposal', () => LoginScreen, store, Provider);
+
+
+
+
   Navigation.registerComponent('example.LoginScreen', () => LoginScreen, store, Provider);
   Navigation.registerComponent('example.FirstTabScreen', () => FirstTabScreen, store, Provider);
   Navigation.registerComponent('example.SecondTabScreen', () => SecondTabScreen, store, Provider);
